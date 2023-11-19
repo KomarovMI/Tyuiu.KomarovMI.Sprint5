9efbedad94bbd9f7760a410ca5e431c848f777ad
Tyuiu.KomarovMI@outlook.com <maximpypsik@mail.ru>
2023-11-19 21:06:38 +0500

    Task3

---
+++ Tyuiu.KomarovMI.Sprint5.Task3.V30.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.KomarovMI.Sprint5.Task3.V30.Lib;
+
+namespace Tyuiu.KomarovMI.Sprint5.Task3.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckedExistsFile()
+        {
+            DataService ds = new DataService();
+            string path = @" C:\Пользователи\maxim\source\repos\Tyuiu.KomarovMI.Sprint5\Tyuiu.KomarovMI.Sprint5.Task3.V30\bin\Debug\OutPutFileTask3.bin";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KomarovMI.Sprint5.Task3.V30/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomarovMI.Sprint5.Task3.V30.Lib;
+
+namespace Tyuiu.KomarovMI.Sprint5.Task3.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Комаров М.И. | ПКТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Спринт #5                                                                *");
+            Console.WriteLine("* Тема:                                                                   *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Комаров Максим Игоревич | ПКТБ-23-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+            Console.WriteLine("* бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до    *");
+            Console.WriteLine("* трёх знаков после запятой.                                              *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 3;
+            Console.WriteLine("x= " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+
+        }
+    }
+}
