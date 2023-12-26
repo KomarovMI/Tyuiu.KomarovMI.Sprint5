f903e7d88d1485844ed7eaaba84edbd5b0a0552c
Tyuiu.KomarovMI@outlook.com <maximpypsik@mail.ru>
2023-12-26 22:46:37 +0500

    Task 4

---
+++ Tyuiu.KomarovMI.Sprint5.Task4.V11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.KomarovMI.Sprint5.Task4.V11.Lib;
+
+namespace Tyuiu.KomarovMI.Sprint5.Task4.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Комаров М.И. | ПКТБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Спринт #5                                                                *");
+            Console.WriteLine("* Тема:                                                                   *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Комаров Максим Игоревич | ПКТБ-23-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* прочитать значение из файла и подставить вместо Х в формулу. Вычислить  *");
+            Console.WriteLine("* значение по формуле и округлить до 3 знаков после запятой. Вернуть      *");
+            Console.WriteLine("* полученый результат на консоль                                          *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V11.txt";
+
+            Console.WriteLine("Данные находяться в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
